1933b6050e722acc2a0b4d78bd01a36ea4dea05b
AvinashReddySankati <40313560+AvinashReddySankati@users.noreply.github.com>
2023-01-29 16:23:48 -0500

    Spacy Dependency Tag
    Spacy Dependency Tag

--- spacyPOSTagging.py
+++ spacyPOSTagging.py
@@ -1,7 +1,7 @@
 #spacy model load 
 import spacy
 #loading english model
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm')
 doc = nlp("Simplilearn is one of the world's leading certification providers.")
 #printing POS tag for tokens
 for token in doc:
